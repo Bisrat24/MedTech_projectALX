17d494d5782f225837c9573741c55ff92837fb89
Bisrat24 <bisrattam@gmail.com>
2023-04-21 21:18:34 +0300

    enable pharmacy registry

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('pharmacy_register/<int:user>/', views.pharmacy_register, name='pharmacy_register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,25 +1,31 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
-from .models import Profile
-from store.models import *
+from store.forms import StoreForm
+from store.models import Store
+
 from .forms import ProfileForm
+from .models import Profile
 
 
 def register(request):
     form = UserCreationForm()
     tform = ProfileForm()
-    form.fields['username'].widget.attrs['class'] = 'form-control'
-    form.fields['password1'].widget.attrs['class'] = 'form-control'
-    form.fields['password2'].widget.attrs['class'] = 'form-control'
-    tform.fields['type'].widget.attrs['class'] = 'form-control'
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            Profile.objects.create(user_id=user.id, type=request.POST.get('type'))
+            Profile.objects.create(
+                user_id=user.id, type=request.POST.get('type'))
+            if request.POST.get('type') == 'pharmacy':
+                return redirect('pharmacy_register', user.id)
             return redirect('login')
+    form.fields['username'].widget.attrs['class'] = 'form-control'
+    form.fields['password1'].widget.attrs['class'] = 'form-control'
+    form.fields['password2'].widget.attrs['class'] = 'form-control'
+    tform.fields['type'].widget.attrs['class'] = 'form-control'
     return render(request, 'auth/register.html', {'form': form, 'tform': tform, 'title': 'Register'})
 
 
@@ -28,12 +34,35 @@ def login_user(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None and validate_user(user):
             login(request, user)
             return redirect('home')
+        else:
+            return render(request, 'auth/login.html', {'error': 'Invalid credentials or Profile not approved', 'title': 'Login'})
     return render(request, 'auth/login.html', {'title': 'Login'})
 
 
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+def validate_user(user):
+    if user.profile.type == 'pharmacy' and not user.store.is_approved:
+        return False
+    return True
+
+
+def pharmacy_register(request, user):
+    form = StoreForm()
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        usr = User.objects.get(id=user)
+        if form.is_valid() and usr is not None and usr.profile.type == 'pharmacy':
+            Store.objects.create(
+                owner=usr, name=request.POST.get('name'), location=request.POST.get('location'), phone=request.POST.get('phone'))
+            return redirect('login')
+    form.fields['name'].widget.attrs['class'] = 'form-control'
+    form.fields['location'].widget.attrs['class'] = 'form-control'
+    form.fields['phone'].widget.attrs['class'] = 'form-control'
+    return render(request, 'auth/pharmacy_register.html', {'form': form, 'title': 'Register Pharmacy'})
