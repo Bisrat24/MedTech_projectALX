c33b51a639c045a2fd013a3662b63448155ce795
Bisrat24 <bisrattam@gmail.com>
2023-04-23 21:07:08 +0300

    add home screen view

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from store.forms import StoreForm
 from store.models import Store
+from drugs.models import Drugs
 
 
 def home(request):
+    if request.user.is_authenticated:
+        drugs = Drugs.objects.all()
+        stores = Store.objects.all()
+        return render(request, "home.html", {"drugs": drugs, "stores": stores})
     return render(request, "home.html")
